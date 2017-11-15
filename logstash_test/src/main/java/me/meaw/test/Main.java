a0a6b193da20b621cc0e812b6b6a6809aa8c09f9
Denis Svetlichniy <shadoff2@gmail.com>
2017-11-15 23:27:49 +0300

    init

---
+++ logstash_test/src/main/java/me/meaw/test/Main.java
@@ -0,0 +1,15 @@
+package me.meaw.test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class Main {
+	private static final Logger logger = LoggerFactory.getLogger(Main.class.getName());
+
+
+	public static void main(String[] args) throws InterruptedException {
+		logger.info("Hello world!");
+	}
+
+}
